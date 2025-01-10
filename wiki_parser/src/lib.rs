2eb2e68227bab210524fd8f6b3ef70e1fb5d5ba5
Bluemi <bruno.schilling@protonmail.ch>
2025-01-10 09:59:45 +0100

    wiki parser returns 0 or 1 to indicate headers

--- wiki_parser/src/lib.rs
+++ wiki_parser/src/lib.rs
@@ -4,13 +4,7 @@ use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use crate::parser_wiki_de::{tokenize, Token};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-fn tokens_to_vec(tokens: &Vec<Token>) -> Vec<String> {
+fn tokens_to_vec(tokens: &Vec<Token>) -> Vec<(u8, String)> {
     let mut parts = Vec::new();
     let mut current_part = String::new();
 
@@ -21,26 +15,26 @@ fn tokens_to_vec(tokens: &Vec<Token>) -> Vec<String> {
             }
             Token::Paragraph | Token::Newline => {
                 if !current_part.is_empty() {
-                    parts.push(current_part);
+                    parts.push((0, current_part));
                     current_part = String::new();
                 }
             }
             Token::Header { text, .. } => {
                 if !current_part.is_empty() {
-                    parts.push(current_part);
+                    parts.push((0, current_part));
                     current_part = String::new();
                 }
                 if matches!(text.to_lowercase().trim(), "einzelnachweise" | "literatur" | "weblinks") {
                     break;
                 }
-                parts.push(text.to_string());
+                parts.push((1, text.to_string()));
             }
             Token::Link(content) => {
                 current_part.push_str(content);
             }
             Token::UnorderedListEntry { tokens, .. } => {
                 if !current_part.is_empty() {
-                    parts.push(current_part);
+                    parts.push((0, current_part));
                     current_part = String::new();
                 }
                 let sub_parts = tokens_to_vec(tokens);
@@ -48,7 +42,7 @@ fn tokens_to_vec(tokens: &Vec<Token>) -> Vec<String> {
             }
             Token::OrderedListEntry { tokens, .. } => {
                 if !current_part.is_empty() {
-                    parts.push(current_part);
+                    parts.push((0, current_part));
                     current_part = String::new();
                 }
                 let sub_parts = tokens_to_vec(tokens);
@@ -62,13 +56,13 @@ fn tokens_to_vec(tokens: &Vec<Token>) -> Vec<String> {
         }
     }
     if !current_part.is_empty() {
-        parts.push(current_part);
+        parts.push((0, current_part));
     }
     parts
 }
 
 #[pyfunction]
-fn parse_wiki(text: &str) -> PyResult<Vec<String>> {
+fn parse_wiki(text: &str) -> PyResult<Vec<(u8, String)>> {
     match tokenize(text) {
         Ok(result) => {
             Ok(tokens_to_vec(&result))
@@ -82,7 +76,6 @@ fn parse_wiki(text: &str) -> PyResult<Vec<String>> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn wiki_parser(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(parse_wiki, m)?)?;
     Ok(())
 }
