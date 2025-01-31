197df06ed4ae04efe8237a2144869f31dbda7e50
Bluemi <bruno.schilling@protonmail.ch>
2025-01-31 13:23:14 +0100

    modified data paths

--- download-wde.sh
+++ download-wde.sh
@@ -4,6 +4,6 @@
 # wget -O "data/dewiki-latest-pages-articles.xml.bz2" "https://dumps.wikimedia.org/dewiki/latest/dewiki-latest-pages-articles.xml.bz2"
 
 # download some artices
-wget -O "data/dewiki-latest-pages-articles-multistream1.xml-p1p297012.bz2" "https://dumps.wikimedia.org/dewiki/latest/dewiki-latest-pages-articles-multistream1.xml-p1p297012.bz2"
+wget -O "data/input/raw/dewiki-latest-pages-articles-multistream1.xml-p1p297012.bz2" "https://dumps.wikimedia.org/dewiki/latest/dewiki-latest-pages-articles-multistream1.xml-p1p297012.bz2"
 # unpack
-bzip2 -dk "data/dewiki-latest-pages-articles-multistream1.xml-p1p297012.bz2"
+bzip2 -dk "data/input/raw/dewiki-latest-pages-articles-multistream1.xml-p1p297012.bz2"
